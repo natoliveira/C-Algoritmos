14903f006a3307fd2a026bc25a18a5d2782aa63d
natoliveira <nataliadeolive@gmail.com>
2022-08-10 11:13:06 -0300

    novos algoritmos

---
+++ somaForcaBruta.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+//custo: O(n.m)
+using namespace std;
+
+int Soma(vector<vector<int>> v){
+    int soma = 0;
+for(int i = 0; i < v.size(); i++){
+    for(int j = 0; j < v[i].size(); j++){
+        cout << v[i][j] << " ";
+    }
+}
+return soma;
+}
+int main()
+{
+    int soma;
+    vector<vector<int>> matriz = {{1,2,3}, {4,5,6}, {7,8, 9}};
+    cout << Soma << " " ;
+
+    return 0;
+}
